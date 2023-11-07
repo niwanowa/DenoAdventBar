795eb3c24fad655d0e411f0c388a13418e8d0eef
niwanowa <40478818+niwanowa@users.noreply.github.com>
2023-11-07 19:11:15 +0900

    プログレスバー表示処理実装

--- main.ts
+++ main.ts
@@ -1,39 +1,75 @@
 import { MultiProgressBar } from "https://deno.land/x/progress@v1.3.9/mod.ts";
 
-const title = "download files";
+const title = "Advent Calendar Progress";
 const total = 100;
+
+// progressオブジェクトの型を定義
+interface ProgressFromJson {
+  title: string;
+  progress: string;
+}
+
+interface Progress {
+  title: string;
+  progress: number;
+}
+
+// 文字列と進行状況の値のマッピングの型を定義
+interface ProgressMapping {
+  [key: string]: number;
+}
+
+// 文字列と進行状況の値のマッピング
+const progressMapping: ProgressMapping = {
+  "タイトル未定": 0,
+  "タイトルだけ決まった": 33,
+  "書いてる": 66,
+  "書いた": 100,
+};
 
 const bars = new MultiProgressBar({
   title,
   // clear: true,
   complete: "=",
   incomplete: "-",
-  display: "[:bar] :text :percent :time :completed/:total",
+  display: "[:bar] :text :percent :completed/:total",
 });
 
-let completed1 = 0;
-let completed2 = 0;
+let completed = 0;
 
-function downloading() {
-  if (completed1 <= total || completed2 <= total) {
-    completed1 += 1;
-    completed2 += 2;
-    bars.render([
-      {
-        completed: completed1,
-        total,
-        text: "file1",
-        // You can also change the style of the progress bar
-        // complete: "*",
-        // incomplete: ".",
-      },
-      { completed: completed2, total, text: "file2" },
-    ]);
+// get progress grom progress.json
+function getProgressFromJson(): Progress[] {
+  const decoder = new TextDecoder("utf-8");
+  const data = Deno.readFileSync("./progress.json");
+  const json = JSON.parse(decoder.decode(data));
+  // progressオブジェクトを型付け
+  const progressInfo: Progress[] = Object.entries(json).map(([day, progressData]: [string, unknown]) => {
+    const progress = progressData as ProgressFromJson;
+    return {
+      title: day,
+      progress: progressMapping[progress.progress],
+    };
+  });
 
+  return progressInfo;
+}
+
+function showProgress(progress: Progress[]) {
+  if (completed <= total){
+    completed += 1;
+    bars.render(
+      progress.map(p => ({ completed:(p.progress >= completed) ? completed:p.progress, total,  text: p.title }))
+    )
     setTimeout(function () {
-      downloading();
+      showProgress(progress);
     }, 100);
   }
 }
 
-downloading();
+function main() {
+  const progress = getProgressFromJson();
+  showProgress(progress);
+}
+
+main();
+console.log("done");
